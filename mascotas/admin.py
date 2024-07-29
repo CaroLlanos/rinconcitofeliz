4e67dffd9b7bff51814fcbd8d6b6c636cbbdcc0e
CaroLlanos <andreallr1999@gmail.com>
2024-07-28 20:21:39 -0400

    Registro de modelos en admin

--- mascotas/admin.py
+++ mascotas/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Mascota
+from .models import Vacuna
+from .models import Adopcion
+from .models import Adoptante
 
 # Register your models here.
+
+"""
+Mascota
+"""
+class MascotaAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "especie", "edad", "color", "para_adopcion",)
+    search_fields = ["nombre"]
+    list_filter = ("para_adopcion",)
+
+admin.site.register(Mascota, MascotaAdmin)
+
